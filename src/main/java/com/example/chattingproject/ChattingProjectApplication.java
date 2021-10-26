81ac6d2e151cc1974d7e91270b62b62cd8a966e6
hyunguk <hyunguk119@gmail.com>
2021-10-26 21:37:22 +0900

    Initial commit

--- src/main/java/com/example/chattingproject/ChattingProjectApplication.java
+++ src/main/java/com/example/chattingproject/ChattingProjectApplication.java
@@ -1,8 +1,14 @@
 package com.example.chattingproject;
 
+import io.swagger.annotations.SwaggerDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@SwaggerDefinition
+@EnableSwagger2
+@Configuration
 @SpringBootApplication
 public class ChattingProjectApplication {
 
